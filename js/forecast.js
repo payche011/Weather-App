bf18a0e6c3ed57de152028fdd7ac3185d9f0a0b8
Pavle Marinkovic <payche255@gmail.com>
2021-12-20 23:15:51 +0100

    weather app

---
+++ js/forecast.js
@@ -0,0 +1,31 @@
+const key = "E2dljAk3Vu0AutHvpR4FE9EiAH4e43NL";
+// get wheater info
+const getWeather = async (id) => {
+  const base = "http://dataservice.accuweather.com/currentconditions/v1/";
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+// get city info
+const getCity = async (city) => {
+  const base = "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
+  return data[0];
+};
+
+getCity("belgrade")
+  .then((data) => {
+    return getWeather(data.Key);
+  })
+  .then((data) => {
+    // console.log(data);
+  })
+  .catch((err) => console.log(err));
